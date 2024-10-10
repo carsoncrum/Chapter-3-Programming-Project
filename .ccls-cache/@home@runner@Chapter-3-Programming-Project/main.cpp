44cd408d2d0241308ad6d5d004cc5d335fc0057f
carsoncrum <carsoncrum07@gmail.com>
2024-10-10 20:35:18 +0000

    Added code

--- .ccls-cache/@home@runner@Chapter-3-Programming-Project/main.cpp
+++ .ccls-cache/@home@runner@Chapter-3-Programming-Project/main.cpp
@@ -1,5 +1,33 @@
-// This is the main branch for 3 separate programs.
+// Calculating number of ingredients needed
 #include <iostream>
 using namespace std;
 
-int main() { cout << "Chapter 3 Programming Project\n"; }
+int main()
+
+{
+  // Original amounts
+  double sugar = 1.5;
+  double butter = 1;
+  double flour = 2.75;
+  int cookiesPerRecipe = 48;
+
+  int desiredCookies;
+  cout << "How many cookies do you wish to make? ";
+  cin >> desiredCookies;
+  
+  double sugarPerCookie = sugar / cookiesPerRecipe;
+  double butterPerCookie = butter / cookiesPerRecipe;
+  double flourPerCookie = flour / cookiesPerRecipe;
+  
+  double totalSugar = sugarPerCookie * desiredCookies;
+  double totalButter = butterPerCookie * desiredCookies;
+  double totalFlour = flourPerCookie * desiredCookies;
+  
+  cout << "You will need:" << endl;
+  cout << totalSugar << " cups of sugar" << endl;
+  cout << totalButter << " cups of butter" << endl;
+  cout << totalFlour << " cups of flour" << endl;
+
+
+  return 0;
+}
